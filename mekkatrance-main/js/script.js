2a7d9ed97f288ce090d167000cf28b94dddf0cbd
Victor Feitosa <88910141+TRINIKZ@users.noreply.github.com>
2025-04-22 15:43:50 -0300

    Delete mekkatrance-main directory

--- mekkatrance-main/js/script.js
+++
@@ -1,9 +0,0 @@
-function redpage() {
-    window.location.href = "html/myFiles.html";
-}
-document.getElementById("button").addEventListener("click", redpage);
-
-window.addEventListener("load", () => {
-    const track = document.querySelector(".carousel-track");
-    track.style.animationPlayState = "running";
-  });
