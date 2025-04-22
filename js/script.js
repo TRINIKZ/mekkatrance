e6a827287bf608fde6e57917cd2fcc2aa9dd3a29
Victor Feitosa <88910141+TRINIKZ@users.noreply.github.com>
2025-04-22 15:44:36 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ function redpage() {
     window.location.href = "html/myFiles.html";
 }
 document.getElementById("button").addEventListener("click", redpage);
+
+window.addEventListener("load", () => {
+    const track = document.querySelector(".carousel-track");
+    track.style.animationPlayState = "running";
+  });
