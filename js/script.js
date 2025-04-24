3b00a035cd92cc00b12f5cffb39f96f38467fa61
Victor Feitosa <trinikz@proton.me>
2025-04-24 16:42:20 -0300

    Otimização de imagens e correções no CSS e JS.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function redpage() {
-    window.location.href = "html/myFiles.html";
+  window.location.href = "html/myFiles.html";
 }
+
 document.getElementById("button").addEventListener("click", () => {
   const panel = document.getElementById("model-panel");
   panel.classList.remove("hidden");
@@ -8,24 +9,23 @@ document.getElementById("button").addEventListener("click", () => {
 });
 
 window.addEventListener("scroll", () => {
-    const scrollY = window.scrollY;
-  
-    // Parallax na camada do meio
-    const midLayer = document.querySelector(".home-mid");
-    const midOffset = scrollY * 0.7;
-    midLayer.style.transform = `translate(-50%, calc(-50% + ${midOffset}px))`;
-  
-    // Blur dinâmico na camada base
-    const baseLayer = document.querySelector(".home-mid");
-    if (baseLayer) {
-      const blurAmount = Math.min(scrollY / 50, 10); // máx 10px
-      baseLayer.style.filter = `blur(${blurAmount}px)`;
-    }
-  });
-  
+  const scrollY = window.scrollY;
 
-//carrossel
+  // Parallax na camada do meio
+  const midLayer = document.querySelector(".home-mid");
+  const midOffset = scrollY * 0.7;
+  midLayer.style.transform = `translate(-50%, calc(-50% + ${midOffset}px))`;
+
+  // Blur dinâmico na camada base
+  const baseLayer = document.querySelector(".home-mid");
+  if (baseLayer) {
+    const blurAmount = Math.min(scrollY / 50, 10); // máx 10px
+    baseLayer.style.filter = `blur(${blurAmount}px)`;
+  }
+});
+
+// Carrossel
 window.addEventListener("load", () => {
-    const track = document.querySelector(".carousel-track");
-    track.style.animationPlayState = "running";
-  });
+  const track = document.querySelector(".carousel-track");
+  track.style.animationPlayState = "running";
+});
